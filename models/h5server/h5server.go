52efdc40ba1bb9508214b6ca9e157638d4f76fdd
ququ <1577826189@qq.com>
2017-09-04 11:06:23 +0800

    commit for origin

--- models/h5server/h5server.go
+++ models/h5server/h5server.go
@@ -1,13 +1,18 @@
 package h5server
 
 import (
+	"archive/zip"
+	"bytes"
+	_ "fmt"
+	"io/ioutil"
 	"log"
+	"net"
 	"net/http"
-	"os"
-	_ "strings"
-	"time"
+	"strconv"
+	"strings"
 
 	"../../utils"
+	"golang.org/x/text/encoding/simplifiedchinese"
 )
 
 /*
@@ -22,41 +27,155 @@ import (
 9.启动http服务，失败：结束进程
 */
 
-var (
-	debugLog *log.Logger
-)
+func H5server_main(port string, fileSize int) {
+	var zipData []byte         //zip包内容
+	var prefix string          //index.html文件名前面的部分
+	var conn net.Conn          //
+	var findIndex bool = false //
 
-func init() {
-	fileName := "d:\\test.log"
-	logFile, err1 := os.OpenFile(fileName, os.O_WRONLY|os.O_CREATE|os.O_APPEND, 0666)
-
-	if err1 != nil {
-		log.Fatalln("open file error !")
+	//连接tcp服务
+	conn, err := net.Dial("tcp", "127.0.0.1:"+port)
+	if err != nil {
+		return
 	}
-	debugLog = log.New(logFile, "[Debug]", log.Ldate|log.Ltime|log.Lshortfile)
+	defer conn.Close()
+
+	//获取zip包
+	zipData = make([]byte, fileSize)
+	ret, err := utils.TcpRecv(conn, &zipData)
+	if ret <= 0 {
+		utils.TcpSendError(conn, 1)
+		return
+	}
+
+	//解析zip包
+	reader := bytes.NewReader(zipData)
+	zreader, err := zip.NewReader(reader, int64(len(zipData)))
+	if err != nil {
+		utils.TcpSendError(conn, 2)
+		return
+	}
+
+	//找index.html
+	for _, file := range zreader.File {
+		if file.UncompressedSize64 > 0 {
+			filename := strings.ToLower(file.Name)
+			i := strings.Index(filename, "index.html")
+			if i >= 0 {
+				findIndex = true
+				prefix = filename[:i]
+				break
+			}
+		}
+	}
+	//log.Println(prefix)
+	//Getchar()
+	if !findIndex {
+		utils.TcpSendError(conn, 3)
+		return
+	}
+
+	//准备handle
+	for _, file := range zreader.File {
+		if file.UncompressedSize64 > 0 {
+			filename := strings.ToLower(file.Name)
+			if strings.HasPrefix(filename, prefix) {
+				pattern := "/" + DecodeToGBK(file.Name[len(prefix):])
+				rc, err := file.Open()
+				if err != nil {
+					continue
+				}
+				fileSize := file.UncompressedSize64
+				data, err := ioutil.ReadAll(rc)
+				if err != nil {
+					log.Println(err)
+					continue
+				}
+				log.Println("serving", pattern, len(data), "bytes")
+				http.HandleFunc(pattern, func(w http.ResponseWriter, r *http.Request) {
+					log.Println("acquireing " + pattern)
+					var contentType string
+					if strings.HasSuffix(pattern, ".html") {
+						contentType = "text/html;charset=utf-8"
+					} else {
+						contentType = "application/octet-stream"
+					}
+
+					strRange := r.Header.Get("Range")
+
+					w.Header().Add("Content-Type", contentType)
+					w.Header().Add("Accept-Ranges", "bytes")
+
+					if strRange != "" {
+						strRange = strRange[6:]
+						//debugLog.Println(strRange)
+						posArr := strings.Split(strRange, "-")
+						startPos := 0
+						endPos := 0
+
+						maxReadSize := 64 * 1024
+						if len(posArr) == 1 {
+							startPos, _ = strconv.Atoi(posArr[0])
+
+						} else if len(posArr) == 2 {
+							startPos, _ = strconv.Atoi(posArr[0])
+							endPos, _ = strconv.Atoi(posArr[1])
+						}
+						//debugLog.Println(len(posArr), startPos, endPos)
+						if endPos == 0 {
+							endPos = startPos + maxReadSize
+							if endPos > len(data) {
+								endPos = len(data)
+							}
+						} else {
+							endPos = endPos + 1
+						}
+
+						size := endPos - startPos
+						if size > 0 {
+							//w.Header().Add("Accept-Ranges", "bytes")
+							w.Header().Add("Content-Length", strconv.Itoa(size))
+							w.Header().Add("Content-Range", "bytes "+strconv.Itoa(startPos)+"-"+strconv.Itoa(endPos-1)+"/"+strconv.Itoa(int(fileSize)))
+							w.WriteHeader(206)
+						}
+						//w.Header().Add("Content-Type", contentType)
+
+						w.Write(data[startPos:endPos])
+						//debugLog.Println("sent data---", startPos, endPos, w.Header())
+
+					} else {
+						w.Header().Add("Content-Length", strconv.Itoa(int(fileSize)))
+						w.Write(data)
+						//debugLog.Println("sent data all", len(data), fileSize, w.Header())
+					}
+				})
+			}
+		}
+	}
+
+	//申请一个端口
+	httpPort := utils.TcpNewPort()
+	if len(httpPort) <= 0 {
+		utils.TcpSendError(conn, 4)
+		return
+	}
+
+	utils.TcpSendHttpPort(conn, httpPort)
+
+	err = http.ListenAndServe("127.0.0.1:"+httpPort, nil)
+	if err != nil {
+		return
+	}
 }
 
-func H5server_main(port string, fileSize int) {
+func DecodeToGBK(text string) string /*, error*/ {
 
-	var zipH ZipHandle
-	zipH.Init(port, int64(fileSize), 64*1024)
-	errParse := zipH.GetParseInfo()
-	if errParse != nil {
-		return
-	}
-	newPort := utils.TcpNewPort()
-	url := "http://127.0.0.1:" + newPort
-	zipH.TcpSendData("url:" + url)
-	log.Println(url)
-	srv := &http.Server{
-		Handler:      &zipH,
-		Addr:         "127.0.0.1:" + newPort,
-		WriteTimeout: time.Second * 5,
-		ReadTimeout:  time.Second * 5,
-	}
-	err := srv.ListenAndServe() //设置监听的端口
-	if err != nil {
-		debugLog.Println("H5server_main---ListenAndServe error---", err)
-		return
-	}
+	dst := make([]byte, len(text)*2)
+	tr := simplifiedchinese.GB18030.NewDecoder()
+	nDst, _, _ := tr.Transform(dst, []byte(text), true)
+	//	if err != nil {
+	//		return text, err
+	//	}
+
+	return string(dst[:nDst] /*, nil*/)
 }
